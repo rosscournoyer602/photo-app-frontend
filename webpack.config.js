b51e8b7db80953260514ac24ab7d9bdd55df3d86
Ross Cournoyer <rcoupvc@yahoo.com>
2024-07-10 08:19:16 -0700

    Add production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const webpack = require('webpack');
-const dotenv = require('dotenv');
+const path = require("path");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
 
 module.exports = () => {
   const env = dotenv.config().parsed;
@@ -13,43 +13,43 @@ module.exports = () => {
   }
 
   return {
-    entry: ['babel-polyfill', './src/index.js'],
-    mode: 'development',
+    entry: ["babel-polyfill", "./src/index.js"],
+    mode: process.env.NODE_ENV,
     module: {
       rules: [
         {
           test: /\.(js|jsx)$/,
           exclude: /(node_modules|bower_components)/,
-          loader: 'babel-loader',
-          options: { presets: ['@babel/env', '@babel/preset-react'] },
+          loader: "babel-loader",
+          options: { presets: ["@babel/env", "@babel/preset-react"] },
         },
         {
           test: /\.(css|scss)$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
+          use: ["style-loader", "css-loader", "sass-loader"],
         },
         {
           test: /\.(svg|eot|woff|woff2|ttf)$/,
-          use: ['file-loader'],
+          use: ["file-loader"],
         },
       ],
     },
-    resolve: { extensions: ['*', '.js', '.jsx'] },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
     output: {
-      path: path.resolve(__dirname, 'dist/'),
-      publicPath: '/',
-      filename: 'bundle.js',
+      path: path.resolve(__dirname, "dist/"),
+      publicPath: "/",
+      filename: "bundle.js",
     },
     devServer: {
-      contentBase: path.join(__dirname, 'public/'),
+      contentBase: path.join(__dirname, "public/"),
       port: 3000,
-      publicPath: 'http://localhost:3000/dist/',
+      publicPath: "http://localhost:3000/dist/",
       // hotOnly: true,
       historyApiFallback: true,
     },
     plugins: [
       new webpack.DefinePlugin(envKeys),
       new webpack.ProvidePlugin({
-        process: 'process/browser',
+        process: "process/browser",
       }),
     ],
   };
